18f7a4df740e0f2fedcb8eb1a1cf43d9770eb866
Juan8516 <juan.ind85@gmail.com>
2023-12-27 15:59:35 -0500

    feat(Creacion): :construction: Creacion componentes de registro.

---
+++ src/app/registration/registrer/registrer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrerer-register',
+  templateUrl: './registrer.component.html',
+  styleUrl: './registrer.component.css'
+})
+export class RegistrerComponent {
+
+}
