24375be2f3517fe291877e7c34ef165998800468
Juan8516 <juan.ind85@gmail.com>
2023-12-29 19:38:39 -0500

    feat(Creacion): :sparkles: Se crea funcion para camboar edad y nombre desde botones.

--- src/app/registration/registrer/registrer.component.ts
+++ src/app/registration/registrer/registrer.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrl: './registrer.component.css'
 })
 export class RegistrerComponent {
+  public name: string = 'Juan Manuel';
+  public age: number = 38;
+
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeName():void {
+    this.name = "Simon";
+  }
+
+  changeAge():void {
+    this.age = 3;
+  }
 
 }
